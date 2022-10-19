9804d2d7d55a9eaca219ea7bde25d4e35cf8f0da
elwyn-de-neve <elwyn_de_neve@hotmail.com>
2022-10-19 10:41:19 +0200

    added initial value to inputRef

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-import './styles/global.css';
+import "./styles/global.css";
+import { useMemo, useRef, useState } from "react";
 
 function App() {
-  return (
-    <>
-    </>
-  );
+    const [ items, setItems ] = useState( [] );
+    const [ query, setQuery ] = useState( "" );
+    const inputRef = useRef( null );
+
+    const filteredItems = useMemo( () => {
+        return items.filter( item => {
+            return item.toLowerCase().includes( query.toLowerCase() );
+        })
+    } ,[ items, query]);
+
+    const onSubmitHandler = ( e ) => {
+        e.preventDefault();
+        const value = inputRef.current.value;
+        if ( value === "" ) return;
+        setItems( prev => {
+            return [ ...prev, value ];
+        } );
+        inputRef.current.value = "";
+    };
+
+    return (
+        <>
+            Search: <input value={ query } onChange={ e => setQuery( e.target.value ) } type="search"/>
+            <br/>
+            <br/>
+            <form onSubmit={ onSubmitHandler }>
+                New Item: <input ref={ inputRef } type="text"/>
+                <button type="submit">add</button>
+            </form>
+            <br/>
+            <h3>Items: </h3>
+            { filteredItems.map( item => (
+                <p>{ item }</p>
+            ) ) }
+        </>
+    );
 }
 
 export default App;
